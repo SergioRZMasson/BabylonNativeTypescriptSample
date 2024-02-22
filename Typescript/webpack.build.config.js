cf6c3b818894126c7d87c9bcaf44ccb992d749f0
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2024-02-22 15:29:52 -0300

    Updated webpack.config

--- Typescript/webpack.build.config.js
+++ Typescript/webpack.build.config.js
@@ -1,23 +1,8 @@
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
-  mode: "production",
-  watch: false,
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-  },
+// mode: "production", // Enable this when shipping for better size.
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
@@ -30,10 +15,6 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            plugins: [
-              require.resolve("@babel/plugin-transform-optional-chaining"),
-              require.resolve("@babel/plugin-transform-nullish-coalescing-operator"),
-            ],
             presets: [['@babel/preset-env', { targets: "ie 11" }]]
           },
         },
